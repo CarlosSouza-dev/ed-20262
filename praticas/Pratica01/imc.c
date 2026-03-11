00775cf78520d7c8e7127989eaffba2c7f7d183f
Carlos <carloseduardo33954580@gmail.com>
2026-03-10 22:16:48 -0300

    imc concluído

--- praticas/Pratica01/imc.c
+++ praticas/Pratica01/imc.c
@@ -2,16 +2,25 @@
 
 int main()
 {
-    int peso;
-    int altura;
-    int IMC;
+    float peso;
+    float altura;
+    float IMC;
     printf("Digite seu peso: \n");
-    scanf("%.2f", &peso);
+    scanf("%f", &peso);
     printf("Digite sua altura em metros: \n");
-    scanf("%.2f", &altura);
-    IMC = peso/altura^2;
+    scanf("%f", &altura);
+    IMC = peso / (altura * altura);
     printf("Seuu IMC: %.2f \n", IMC);
     
+    if (IMC < 18.5){
+        printf("abaixo do peso");
+    } else if (18.5 <= IMC && IMC <= 24.5){
+            printf("peso normal");
+        } else if (25.0 <= IMC && IMC <= 29.9){
+            printf("sobrepeso");
+        } else if (IMC > 30.0){
+            printf("obesidade");
+        }
 
     return 0;
 }
