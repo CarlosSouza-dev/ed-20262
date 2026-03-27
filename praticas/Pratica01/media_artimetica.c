6bbbc7888d8bcc86053fb89ac8fb71dc91ffe472
Carlos <carloseduardo33954580@gmail.com>
2026-03-26 23:44:49 -0300

    feat: conclui pratica01corrigido. Fecha #ID

--- praticas/Pratica01/media_artimetica.c
+++ praticas/Pratica01/media_artimetica.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-#include <windows.h>
 float calcula_media_aritmetica(int n){
     float soma = 0.0f;
     float vet[100];
     for (int i = 0; i < n; i++){
-        printf("n = %d, informe o %dº número: ", n, i + 1);
+        printf("n = %d, informe o %dº numero: ", n, i + 1);
             scanf("%f", &vet[i]);
             soma += vet[i];
         }
@@ -13,22 +12,21 @@ float calcula_media_aritmetica(int n){
  
 int main()
 {
-    SetConsoleOutputCP(65001);
     
     float media = calcula_media_aritmetica(3);
-    printf("A média aritmética é: %.2f\n", media);   
+    printf("A media aritmetica e: %.2f\n", media);   
     printf("\n");
 
     media = calcula_media_aritmetica(4);
-    printf("A média aritmética é: %.2f\n", media);   
+    printf("A media aritmetica e: %.2f\n", media);   
     printf("\n");
     
     media = calcula_media_aritmetica(5);
-    printf("A média aritmética é: %.2f\n", media);   
+    printf("A media aritmetica e: %.2f\n", media);   
     printf("\n");
 
     media = calcula_media_aritmetica(6);
-    printf("A média aritmética é: %.2f\n", media);   
+    printf("A media aritmetica e: %.2f\n", media);   
 
     return 0;
 }
